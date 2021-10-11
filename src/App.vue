405c5bdce3dc67c425798770176c20adc8c49791
Павел <pavel.plotnikov1999@mail.ru>
2021-10-11 08:56:49 +0700

    FEAT: Добавил авторизацию и изменил дизайн

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,219 @@
 <template>
   <v-app>
+    <div v-if="!authorize">
+      <v-row justify="center">
+        <v-col md="3">
+          <v-img
+              src="../public/soulsLike.jpg"
+              lazy-src="../public/soulsLike.jpg"
+              max-height="800"
+              max-width="600"
+          >
+          </v-img>
+        </v-col>
+      </v-row>
 
-    <v-navigation-drawer
-        app
-        left
-        temporary
-        v-model="drawer"
-        color="grey darken-4"
-    >
+      <v-row justify="center">
+        <v-col md="1">
+          <v-dialog
+              transition="dialog-bottom-transition"
+              max-width="600"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                  class="ma-2"
+                  outlined
+                  color="white"
+                  v-bind="attrs"
+                  v-on="on"
+              >Вход
+              </v-btn>
+            </template>
+            <template v-slot:default="dialog">
+              <v-card>
+                <v-toolbar
+                    color="grey darken-3"
+                    dark
+                >Вход
+                </v-toolbar>
+                <v-card-text>
+                  <!--форма-->
+                  <div>
+                    <v-text-field
+                        label="Логин"
+                        v-model="username"
+                    ></v-text-field>
+                    <v-text-field label="Пароль" v-model="password"></v-text-field>
+                  </div>
+                </v-card-text>
+                <v-card-actions class="justify-end">
+                  <v-btn
+                      text
+                      @click="login"
+                  >Войти
 
-      <v-list-item-group color="white">
+                  </v-btn>
 
-        <v-list-item v-for="link in links" :key="link.id">
-          <v-list-item-title><router-link class="link" :to="link.href">{{link.name}}</router-link></v-list-item-title>
-        </v-list-item>
+                  <v-btn
+                      text
+                      @click="dialog.value = false"
+                  >Закрыть
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </template>
+          </v-dialog>
+        </v-col>
 
-      </v-list-item-group>
+        <v-col md="1">
+          <v-dialog
+              transition="dialog-bottom-transition"
+              max-width="600"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                  class="ma-2"
+                  outlined
+                  color="white"
+                  v-bind="attrs"
+                  v-on="on"
+              >Регистрация
+              </v-btn>
+            </template>
+            <template v-slot:default="dialog">
+              <v-card>
+                <v-toolbar
+                    color="grey darken-3"
+                    dark
+                >Регистрация
+                </v-toolbar>
+                <v-card-text>
+                  <!--форма-->
+                  <div>
+                    <v-text-field
+                        label="Логин"
+                        v-model="username"
+                    ></v-text-field>
+                    <v-text-field label="Пароль" v-model="password"></v-text-field>
+                  </div>
+                </v-card-text>
+                <v-card-actions class="justify-end">
+                  <v-btn
+                      text
+                      @click="register"
+                  >Регистрация
 
-    </v-navigation-drawer>
+                  </v-btn>
 
-    <v-app-bar app color="grey darken-4">
-      <v-app-bar-nav-icon @click="drawer = !drawer" color="white"></v-app-bar-nav-icon>
-      <v-app-bar-title>
-        <h3 class="text-h4 white--text">Dark Souls party</h3>
-      </v-app-bar-title>
+                  <v-btn
+                      text
+                      @click="dialog.value = false"
+                  >Закрыть
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </template>
+          </v-dialog>
+        </v-col>
 
-    </v-app-bar>
+      </v-row>
 
-    <v-main>
-      <router-view></router-view>
-    </v-main>
+    </div>
+    <div v-if="authorize">
+      <v-navigation-drawer
+          app
+          left
+          temporary
+          v-model="drawer"
+          color="grey darken-4"
+      >
 
+        <v-list-item-group color="white">
+
+          <v-list-item v-for="link in links" :key="link.id">
+            <v-list-item-title>
+              <router-link class="link" :to="link.href">{{ link.name }}</router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title @click="logout">
+               Выход
+            </v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+
+      </v-navigation-drawer>
+
+      <v-app-bar app color="grey darken-4">
+        <v-app-bar-nav-icon @click="drawer = !drawer" color="white"></v-app-bar-nav-icon>
+        <v-app-bar-title>
+          <h3 class="text-h4 white--text">Dark Souls party</h3>
+        </v-app-bar-title>
+
+      </v-app-bar>
+
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </div>
   </v-app>
+
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data: () => ({
-    drawer: false,
-    links: [
-      {
-        id:1,
-        name:'Список комнат',
-        href:'/',
-      },
-      {
-        id:2,
-        name:'Создать комнату',
-        href:'/createRoom',
-      }
-    ]
-  }),
   mounted() {
     document.title = 'Dark Souls';
+    this.$store.dispatch('GET_CATALOG');
+  },
+  methods:{
+    register(){
+      this.$store.dispatch('REGISTER');
+    },
+    login(){
+      this.$store.dispatch('LOGIN');
+    },
+    logout(){
+      this.$store.commit('setAuthorize',false);
+      this.$store.commit('setToken', "");
+    }
+  },
+  computed: {
+    authorize() {
+      return this.$store.state.authorize;
+    },
+    username: {
+      get() {
+        return this.$store.state.username;
+      },
+      set(value) {
+        this.$store.commit('setUserName', value);
+      }
+    },
+    password: {
+      get() {
+        return this.$store.state.password;
+      },
+      set(value) {
+        this.$store.commit('setPassword', value);
+      }
+    },
+    drawer:{
+      get() {
+        return this.$store.state.drawer;
+      },
+      set(value) {
+        this.$store.commit('setDrawer', value);
+      }
+    }
   }
 }
 </script>
 <style>
-.link{
-  text-decoration:none;
-  color:white;
+.link {
+  text-decoration: none;
+  color: white;
 }
 </style>
