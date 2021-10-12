2060824b4da7f1923c5449471692f050334dfa79
Павел <pavel.plotnikov1999@mail.ru>
2021-10-12 10:02:25 +0700

    FEAT: Форма создания поста

--- src/components/createRoom.vue
+++ src/components/createRoom.vue
@@ -6,21 +6,145 @@
       </v-col>
     </v-row>
     <v-row class="list__cafes-title">
-      <v-autocomplete
-          filled
-          rounded
-          :items="items"
-          label="Введите для поиска"
-      ></v-autocomplete>
+      <v-col md="12">
+        <v-row>
+          <v-col md="12">
+            <v-text-field
+                label="Тип поста"
+                hide-details="auto"
+                filled
+                rounded
+                v-model="createRoom.postType"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col md="12">
+            <v-select
+                filled
+                rounded
+                :items="games"
+                item-text="name"
+                item-value="id"
+                label="Выберите игру"
+                v-model="createRoom.game"
+            ></v-select>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col md="12">
+            <v-select
+                filled
+                rounded
+                :items="filteredPlatforms"
+                item-text="name"
+                item-value="id"
+                label="Выберите платформу"
+                v-model="createRoom.platform"
+            ></v-select>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col md="12">
+            <v-select
+                filled
+                rounded
+                :items="filteredLocationTypes"
+                item-text="name"
+                item-value="id"
+                label="Выберите тип локации"
+                v-model="createRoom.locationType"
+            ></v-select>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col md="12">
+            <v-select
+                filled
+                rounded
+                :items="filteredLocationTypes"
+                item-text="name"
+                item-value="id"
+                label="Выберите тип локации"
+                v-model="createRoom.locationType"
+            ></v-select>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col md="12">
+            <v-select
+                filled
+                rounded
+                :items="filteredLocations"
+                item-text="name"
+                item-value="id"
+                label="Выберите локация"
+                v-model="createRoom.location"
+            ></v-select>
+          </v-col>
+        </v-row>
+
+        <v-row justify="center">
+          <v-col md="1" >
+            <v-btn @click="createPost">
+              Создать
+            </v-btn>
+          </v-col>
+        </v-row>
+
+      </v-col>
     </v-row>
+
+
+
+
   </v-container>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-  }),
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    },
+    locationsTypes() {
+      return this.$store.state.locationTypes
+    },
+    locations() {
+      return this.$store.state.locations
+    },
+    platforms() {
+      return this.$store.state.platforms
+    },
+    games() {
+      return this.$store.state.games
+    },
+    filteredPlatforms() {
+      return this.$store.state.platforms.filter(plat => plat.games.find(game => game === this.createRoom.game))
+    },
+    filteredLocationTypes() {
+      return this.$store.state.locationTypes.filter(type => type.games.find(game => game === this.createRoom.game))
+    },
+    createRoom() {
+      return this.$store.state.createRoom
+    },
+    filteredLocations(){
+      return this.$store.state.locations.filter(locate => locate.game === this.createRoom.game && locate.locationType === this.createRoom.locationType)
+    }
+  },
+  methods: {
+    send() {
+      console.log(this.createRoom.game)
+    },
+    createPost(){
+      this.$store.dispatch('CREATE_POST')
+    }
+  }
 }
 </script>
 
